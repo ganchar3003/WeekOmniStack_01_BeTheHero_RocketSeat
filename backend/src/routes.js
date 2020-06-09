d2dec9368035308379df53bcfe9dbc48c52f92bd
Luis Augusto Santin Ganchar <42579879+ganchar3003@users.noreply.github.com>
2020-06-09 14:11:46 -0300

    FirstCommit

---
+++ backend/src/routes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const ongController = require('./controlers/ongController');
+const IncidentController = require('./controlers/IncidentController');
+const ProfileController = require('./controlers/ProfileController');
+const SessionController = require('./controlers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', ongController.index);
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), ongController.create);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.index);
+
+routes.post('/incidents', IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
+
+module.exports = routes;
